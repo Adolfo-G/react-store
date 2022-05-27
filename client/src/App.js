b29159f725cc689123c393cf4437716f3144af36
Adolfo Gonzalez <agonzal177@gmail.com>
2022-05-27 17:16:40 -0400

    added a ratings and starter product page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home'
+import ProductPage from './pages/ProductPage'
 
 import { Container } from 'react-bootstrap'
 
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <Home/>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/product/:id' element={<ProductPage/>}/>
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
