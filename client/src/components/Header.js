98bd0d4f12b28e2bb29328d2af91585e900947ee
Adolfo Gonzalez <agonzal177@gmail.com>
2022-05-27 18:09:37 -0400

    added individual product page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,12 +5,21 @@ import { LinkContainer } from 'react-router-bootstrap'
 function Header() {
     return (
         <header>
-            <Navbar bg="dark" variant='dark'collapseOnSelect>
+            <Navbar bg="dark" variant='dark' collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>ProShop</Navbar.Brand>
+                    </LinkContainer>
                     <Nav className="ml-auto">
-                        <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-                        <Nav.Link href="/login"><i className='fas fa-user'></i>Sign In</Nav.Link>
+
+                        <LinkContainer to='/cart'>
+                            <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                        </LinkContainer>
+                        
+                        <LinkContainer to='/login'>
+                            <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
+                        </LinkContainer>
+                    
                     </Nav>
                 </Container>
             </Navbar>
