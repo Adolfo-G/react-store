e32ae3ede7930292a588beeeac03fa9de3d0d9a8
Adolfo Gonzalez <agonzal177@gmail.com>
2022-06-01 13:44:21 -0400

    added mongodb and made models for db

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useState, useEffect} from 'react'
 import { Row, Col } from 'react-bootstrap';
-import products from '../products'
-import Product from '../components/Product'
+import Product from '../components/Product';
+
 
 
 function Home(){
+    const [products, setProducts]= useState([])
+    useEffect(()=>{
+        const fetchProducts=async()=>{
+            const {data}= await axios.get('/api/products')
+            setProducts(data)
+        }
+        fetchProducts()
+    },[])
     return(
         <>
             <h1>Latest Products</h1>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
+const connectDB = require('./config/connection')
 
+dotenv.config()
+connectDB()
 const app = express()
 
 app.get('/', (req, res)=>{
@@ -16,4 +20,6 @@ app.get('/api/products/:id', (req, res)=>{
     res.json(product)
 })
 
-app.listen(5000, console.log("server is running"))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}.`))
